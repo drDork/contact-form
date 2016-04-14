a5de936638a1bce2def1f8bfca4470653f1df0b3
Ryan Graham <nashgryanb@gmail.com>
2016-04-14 15:46:42 -0600

    form partially finished

--- contact-us.php
+++ contact-us.php
@@ -3,11 +3,39 @@
 
 <head> 
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Suggestions Wanted</title>
 </head> 
 
 <body> 
 
-	<?php echo "<p>Hello Fartzilla</p>"; ?>
+  <h1>Share something cool!</h1>
+
+  <div class="wrapper">
+  	<form method="post" action="process.php">
+  		<table>
+  			<tr>
+  				<th><label for="name">Name</label></th>
+  				<td><input type="text" id="name" name="name" /></td> 
+  			</tr>
+
+  			<tr>
+  				<th><label for="email">Email</label></th>
+  				<td><input type="text" id="email" name="email" /></td> 
+  			</tr>
+
+  			<tr>
+  				<th><label for="suggestion">Suggestion</label></th>
+  				<td><textarea name="suggestion" id="suggestion"></textarea></td> 
+  			</tr>
+
+  		</table> 
+
+  		<input type="submit" value="Send" />
+
+  	</form>
+  </div> 
+
+	
 
 
 </body> 
---
+++ process.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST["name"]; 
+$email = $_POST["email"];
+$sug = $_POST["suggestion"];
+
+echo $name; 
+echo $email; 
+echo $sug;
+
+?> 
