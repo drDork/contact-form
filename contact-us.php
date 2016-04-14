02dd0dfa50f57fff9bb6f5109bdfbba4dd988052
Ryan Graham <nashgryanb@gmail.com>
2016-04-14 14:34:55 -0600

    first commit

---
+++ contact-us.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html> 
+
+<head> 
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head> 
+
+<body> 
+
+	<?php echo "<p>Hello Fartzilla</p>"; ?>
+
+</body> 
+
+</html> 
