ad7ee841fd03b4752be38fff4db5d3de16c2399a
Ryan Graham <nashgryanb@gmail.com>
2016-04-18 15:42:59 -0600

    added thanks.php

--- process.php
+++ process.php
@@ -1,46 +1,3 @@
-<!DOCTYPE html> 
-<html> 
-
-<head> 
-
-<link rel="stylesheet" type="text/css" href="style.css">
-
-</head> 
-
-<body> 
-
-<div class="wrapper">
-
-	<h1>Thank you for the suggestion!</h1>
-
-  	<form method="post" action="process.php">
-  		<table>
-  			<tr>
-  				<th><label for="name">Name</label></th>
-  				<td><input type="text" id="name" name="name" /></td> 
-  			</tr>
-
-  			<tr>
-  				<th><label for="email">Email</label></th>
-  				<td><input type="text" id="email" name="email" /></td> 
-  			</tr>
-
-  			<tr>
-  				<th><label for="suggestion">Suggestion</label></th>
-  				<td><textarea name="suggestion" id="suggestion"></textarea></td> 
-  			</tr>
-
-  			<tr> 
-  				<th></th> 
-  				<td><input class="sendbtn" type="submit" value="Send" /></td> 
-  			</tr> 
-
-  		</table> 
-
-  	</form>
-  </div> 
-
-
 <?php
 
 $name = $_POST["name"]; 
@@ -56,11 +13,5 @@ echo $email_body;
 echo "</pre>";
 
 // To Do: Send Email 
-
-$pageTitle = "Thank you"; 
-$section = null;
-
+header("location:thanks.php");
 ?> 
-</body> 
-
-</html> 
